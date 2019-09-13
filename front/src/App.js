e8fd51086a04c319f1ca4f0855a2fc9a91fa7432
fj-mac <fj.mac@uniandes.edu.co>
2019-09-13 16:12:23 -0500

    first commit

---
+++ front/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const App = () =>{
+
+  return(<div className="row">
+      <div className="col-md-8">
+        <h3>lo primero</h3>
+        <p>por aca</p>
+      </div>
+      <div className="col-md-4">
+        <h3>a la derecha </h3>
+        <p>Todo esto</p>
+      </div>
+    </div>
+    );
+}
+
+export default App;
+
